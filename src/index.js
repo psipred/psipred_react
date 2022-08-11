0e08b3c70e6c17562115775c6368a377a9fb4307
Daniel Buchan <clone@myself.com>
2022-08-11 13:00:15 +0100

    seq form ported

--- src/index.js
+++ src/index.js
@@ -2,29 +2,157 @@ import ReactDOM from 'react-dom/client'
 import React from 'react';
 
 
+class SeqForm extends React.Component {
+  render () {
+    return(
+      <div className="form-group">
+        <div className="col-md-12">
+          <div className="row form-header-row"><br />
+          <form className="form" id="main_form" method="Post" intro="slide" outro="slide" name="main_form">
+            <input type="hidden" name="csrfmiddlewaretoken" value="6TwolraqeOHCMgVbxvCgNJ3EQsHdqo9Rbp5GNIZkxWUFhlYjWMfLikgu0x7SxLDa" />
+            <div className="form-group">
+              <tr><td colspan="2"><h4>Popular Analyses</h4></td></tr>
+              <table className="full-width-table">
+                <tr>
+                  <td data-toggle="tool-tip" title="Predict helices, beta sheets and coils from AA sequence"><input type="checkbox" id="id_psipred_job" name="psipred_job" value="psipred_job" />&nbsp;<label for="id_psipred_job" >PSIPRED 4.0 (Predict Secondary Structure)</label></td>
+                  <td data-toggle="tool-tip" title="Detect intrinsically disordered regions in proteins"><input type="checkbox" id="id_disopred_job" name="disopred_job" value="disopred_job" />&nbsp;<label for="id_disopred_job">DISOPRED3 (Disopred Prediction)</label></td>
+                </tr>
+                <tr>
+                  <td data-toggle="tool-tip" title="Calculate length, location and topology of transmembrane helices"><input type="checkbox" id="id_memsatsvm_job" name="memsatsvm_job" value="memsatsvm_job" />&nbsp;<label for="id_memsatsvm_job">MEMSAT-SVM (Membrane Helix Prediction)</label></td>
+                  <td data-toggle="tool-tip" title="Protein fold recognition with protein templates of known structure."><input type="checkbox" id="id_pgenthreader_job" name="pgenthreader_job" value="pgenthreader_job" />&nbsp;<label for="id_pgenthreader_job">pGenTHREADER (Profile Based Fold Recognition)</label></td>
+                </tr>
+                <tr><td className="input_table" colspan="2"><hr /></td></tr>
+                <tr><td colspan="2"><h4>Contact Analysis</h4></td></tr>
+                <tr>
+                  <td data-toggle="tool-tip" title="Predict interresidue contacts using a convolutional neural network"><input type="checkbox" id="id_dmp_job" name="dmp_job" value="dmp_job" />&nbsp;<label for="id_dmp_job">DeepMetaPSICOV 1.0 (Structural Contact Prediction)</label></td>
+                  <td data-toggle="tool-tip" title="Predict packing membrane helices"><input type="checkbox" id="id_mempack_job" name="mempack_job" value="mempack_job" />&nbsp;<label for="id_mempack_job">MEMPACK (TM Topology and Helix Packing)</label></td>
+                </tr>
+                <tr><td className="input_table" colspan="2"><hr /></td></tr>
+                <tr><td><h4>Fold Recognition</h4></td><td></td></tr>
+                <tr>
+                  <td data-toggle="tool-tip" title="Fast fold recognition method using template structures"><input type="checkbox" id="id_genthreader_job" name="genthreader_job" value="genthreader_job" />&nbsp;<label for="id_genthreader_job">GenTHREADER (Rapid Fold Recognition)</label></td>
+                  <td data-toggle="tool-tip" title="Fast protein domain fold recognition using domain templates"><input type="checkbox" id="id_pdomthreader_job" name="pdomthreader_job" value="pdomthreader_job" />&nbsp;<label for="id_pdomthreader_job">pDomTHREADER (Protein Domain Fold Recognition)</label></td>
+                </tr>
+                <tr><td className="input_table" colspan="2"><hr /></td></tr>
+                <tr><td><h4>Structure Modelling</h4></td><td></td></tr>
+                <tr>
+                  <td data-toggle="tool-tip" title="Fully automated homology modelling"><input type="checkbox" id="id_bioserf_job" name="bioserf_job" value="bioserf_job" />&nbsp;<label for="id_bioserf_job">Bioserf 2.0 (Automated Homology Modelling)</label></td>
+                  <td data-toggle="tool-tip" title="Fully automated structural domain homology modelling"><input type="checkbox" id="id_domserf_job" name="domserf_job" value="domserf_job" />&nbsp;<label for="id_domserf_job">Domserf 2.1 (Automated Domain Homology Modelling)</label></td>
+                </tr>
+                <tr>
+                  <td data-toggle="tool-tip" title="Accurate structure prediction using residue-residue contacts."><input type="checkbox" onclick="dmpfoldAlert();" id="id_dmpfold_job" name="dmpfold_job" value="dmpfold_job" />&nbsp;<label for="id_dmpfold_job">DMPfold 1.0 Fast Mode (Protein Structure Prediction)</label></td>
+                  <td></td>
+                </tr>
+                <tr><td className="input_table" colspan="2"><hr /></td></tr>
+                <tr><td><h4>Domain Prediction</h4></td><td></td></tr>
+                <tr>
+                  <td data-toggle="tool-tip" title="Predict protein structural domain boundaries."><input type="checkbox" id="id_dompred_job" name="dompred_job" value="dompred_job" />&nbsp;<label for="id_dompred_job">DomPred (Protein Domain Prediction)</label></td>
+                  <td></td>
+                </tr>
+                <tr><td className="input_table" colspan="2"><hr /></td></tr>
+                <tr><td><h4>Function Prediction</h4></td><td></td></tr>
+                <tr>
+                  <td data-toggle="tool-tip" title="Predict protein function, using Gene Ontology annotations"><input type="checkbox" id="id_ffpred_job" name="ffpred_job" value="ffpred_job" />&nbsp;<label for="id_ffpred_job">FFPred 3 (Eurkaryotic Function Prediction)</label> </td>
+                  <td></td>
+                </tr>
+                <tr><td colspan="2"><a className="form-link" href="http://bioinfadmin.cs.ucl.ac.uk/UCL-CS_Bioinformatics_Server_Tutorial.html">Help...</a><br /></td></tr>
+              </table>
+            </div>
+
+            <div className="col-sm-12 form-header">
+              <div className="row form-header-row">
+                <h5 className="float-header">Submission details</h5>
+              </div>
+            </div>
+            <br /><br /><p className="form_error"></p>
+            <div className="form-group">
+              <div className="form-group">
+                <label className="control-label" for="id_input_data">Protein Sequence</label><textarea className="form-control" cols="40" rows="3" placeholder="Protein Sequence" title="" required="" id="id_input_data" name="input_data" value=""></textarea>
+              </div>
+            </div>
+            <a className="form-link" href="http://bioinfadmin.cs.ucl.ac.uk/UCL-CS_Bioinformatics_Server_Tutorial.html">Help...</a><br /> If you wish to test these services follow this link to retrieve <a className="form-link" href="http://www.uniprot.org/uniprot/B0R5N9.fasta">a test fasta sequence</a>.
+            <br /><br />
+            <div className="form-group">
+              <div className="form-group">
+                <label className="control-label" for="id_job_name">Job name</label><input className="form-control" type="text" placeholder="Job name" title="" required="" id="id_job_name" name="job_name" value="" />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="form-group">
+                <label className="control-label" for="id_email">Email (optional)</label><input className="form-control" type="email" placeholder="Email (optional)" title="" id="id_email" name="email" value="" />
+              </div>
+            </div>
+            <div className="form-group">
+              <input className="btn btn-danger" type="reset" value="Reset" /> <input className="btn btn-primary" type="submit" value="Submit" />
+            </div>
+          </form>
+          </div>
+        </div>
+      </div>);
+  }
+}
+
+class StructForm extends React.Component {
+  render () {
+    return(<div className="form-group">STRUCT FORM</div>);
+  }
+}
+
+
 class FormInteractivity extends React.Component{
-  render(){
+  constructor(props){
+    super(props);
+    this.state = {formSelectedOption: 'SeqForm'};
+  }
+
+  handleInputChange = (event) =>  {
+    this.setState({
+      formSelectedOption: event.target.value,
+    });
+  }
+
+  render() {
     return(
       <div className="box-body">
         <div className="form-group">
-          <div className="col-md-12 form-header form_header_blue">
-            <div className="row form-header-row"><h5 className="float-header">Select input data type</h5>
-            </div>
-          </div>
 
+          { this.state.formSelectedOption === "SeqForm" ?
+            <div className="col-md-12 form-header-blue">
+              <div className="row form-header-row"><h5 className="float-header">Select input data type</h5></div>
+            </div>
+          :
+            <div className="col-md-12 form-header-red">
+              <div className="row form-header-row"><h5 className="float-header">Select input data type</h5></div>
+            </div>
+          }
           <div className="col-md-12">
             <div className="row form-header-row">
               <div className="funkyradio">
-                <div className="funkyradio-primary"><input type="radio" id="radio1" name="radio" value="on" /> <label for="radio1"><b>Sequence Data&nbsp;&nbsp;</b></label></div>&nbsp;&nbsp;&nbsp;
-                <div className="funkyradio-danger"><input type="radio" id="radio2" name="radio" value="on" /> <label for="radio2"><b>PDB Structure Data&nbsp;&nbsp;</b></label></div>
+                <div className="funkyradio-primary"><input type="radio" id="radio1" name="radio" value="SeqForm" checked={this.state.formSelectedOption === "SeqForm"} onChange={this.handleInputChange} /> <label htmlFor="radio1"><b>Sequence Data&nbsp;&nbsp;</b></label></div>&nbsp;&nbsp;&nbsp;
+                <div className="funkyradio-danger"><input type="radio" id="radio2" name="radio" value="StructForm" checked={this.state.formSelectedOption === "StructForm"} onChange={this.handleInputChange} /> <label htmlFor="radio2"><b>PDB Structure Data&nbsp;&nbsp;</b></label></div>
               </div><br />
             </div>
           </div>
 
 
         </div>
-        <div className="form-group"></div>
-        <div className="form-group"></div>
+        <div className="form-group">
+        { this.state.formSelectedOption === "SeqForm" ?
+          <div className="col-md-12 form-header-blue">
+            <div className="row form-header-row">
+              <h5 className="float-header">Choose prediction methods (hover for short description)</h5>
+            </div>
+          </div>
+          :
+          <div className="col-md-12 form-header-red">
+            <div className="row form-header-row">
+              <h5 className="float-header">Choose prediction methods (hover for short description)</h5>
+            </div>
+          </div>
+        }
+        </div>
+        { this.state.formSelectedOption === "SeqForm" ? <SeqForm /> : <StructForm />}
+
       </div>
       );
   }
