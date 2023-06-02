66f0fc3c32df76cb66340af3f069279b2bcfb2b1
Daniel Buchan <clone@myself.com>
2023-06-02 16:53:03 +0100

    refactor to support MSA and model pages and shared functions

---
+++ src/msa/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export class App extends React.Component{
+    constructor(props){
+        super(props);
+        //let things = "thing";
+        this.state = {};
+      }
+
+      render(){
+        console.log('msa');
+        return(
+            <div>
+              <h1>HI</h1>
+            </div>
+        )
+    }
+}
