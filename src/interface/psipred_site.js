0630b94ffd34cdbf6e2cebb4354d84519046d404
Daniel Buchan <clone@myself.com>
2024-02-14 12:50:33 +0000

    testing frontend

--- src/interface/psipred_site.js
+++ src/interface/psipred_site.js
@@ -490,8 +490,8 @@ export class PsipredSite extends React.Component{
     this.state = {
       //suspension_message: null,
       server_message: null,
-      suspension_message: "The server will be offline until the 14th of Feb 2023",
-      //server_message: "We are going to change some shit",
+      //suspension_message: "The server will be offline until the 14th of Feb 2024",
+      server_message: "We are going to change some shit",
       endpoints_url: null,
       submit_url: null,
       times_url: null,
