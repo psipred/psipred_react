1d96cec981fae394ac3d953e6765ebc4b2ccfb86
Daniel Buchan <clone@myself.com>
2025-03-04 15:19:33 +0000

    genth fix

--- src/interface/parsers.js
+++ src/interface/parsers.js
@@ -558,6 +558,7 @@ export function parse_presults(file, ann_list, type)
   let lines = file.split('\n');
   let pseudo_table = null;
   console.log("PARSING ANN LIST");
+  console.log(ann_list);
   console.log(JSON.stringify(ann_list));
   console.log("SHOWN ANN LIST");
   if(Object.keys(ann_list).length > 0){
