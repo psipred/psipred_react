f66afd261fd19e10c41d95f8b8e627273fcfcfda
Daniel Buchan <clone@myself.com>
2025-03-13 14:07:42 +0000

    message

--- src/interface/psipred_site.js
+++ src/interface/psipred_site.js
@@ -549,8 +549,8 @@ export class PsipredSite extends React.Component{
     this.state = {
       suspension_message: null,
       server_message: null,
-      suspension_message: "The server is offline due to a network disk failure",
-      //server_message: "The PSIPRED Workbench is undergoing some server upgrades. We apologise for any service disruption. If you experience any issues please email psipred-help@cs.ucl.ac.uk",
+      //suspension_message: "The server is offline due to a network disk failure",
+      server_message: "The PSIPRED Workbench is undergoing some server upgrades. We apologise for any service disruption. If you experience any issues please email psipred-help@cs.ucl.ac.uk",
       endpoints_url: null,
       submit_url: null,
       times_url: null,
