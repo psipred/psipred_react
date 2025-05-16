2b262c946beb2ebc4b2e9268ea05aaaf26b88543
Daniel Buchan <clone@myself.com>
2025-05-16 17:37:42 +0100

    fix image download

--- src/interface/mainform.js
+++ src/interface/mainform.js
@@ -255,9 +255,11 @@ class FormInteractivity extends React.Component{
               <div className="row form-header-row"><h5 className="float-header">Select input data type</h5></div>
             </div>
           : 
+          {/* 
           <div className="col-md-12 form-header-green">
               <div className="row form-header-row"><h5 className="float-header">Select input data type</h5></div>
             </div>
+            */}
           }
           <div className="col-md-12">
             <div className="row form-header-row">
